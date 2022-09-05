53ef1a51530dfa887d39eefe6e054e657638e9a8
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2022-09-05 23:41:50 +0430

    mu_mix = sample_mu * lmda + cluster_mu * (1-lmda)

--- code/archs/cluster/cluster_mixstyle.py
+++ code/archs/cluster/cluster_mixstyle.py
@@ -67,7 +67,7 @@ class cluster_MixStyle(nn.Module):
         sample_std = ((x.var(dim=[2, 3], keepdim=True) + self.eps).sqrt()).detach()
         
         x_normed = (x-sample_mu) / sample_std            
-        mu_mix = sample_mu * lmda + cluster_mu (1-lmda)
+        mu_mix = sample_mu * lmda + cluster_mu * (1-lmda)
         std_mix = sample_std * lmda + cluster_std * (1-lmda)
 
         return x_normed * std_mix + mu_mix
